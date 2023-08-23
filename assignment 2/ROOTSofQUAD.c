69932077add3b7001417b33a70ac77c207ecdba3
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2023-08-23 22:59:10 +0530

    Add files via upload

---
+++ assignment 2/ROOTSofQUAD.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c;
+    printf("Enter coefficients a, b, and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant >= 0) {
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Roots: %.2lf, %.2lf\n", root1, root2);
+    } else {
+        printf("No real roots.\n");
+    }
+
+    return 0;
+}

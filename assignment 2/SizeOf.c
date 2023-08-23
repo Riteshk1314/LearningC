c2424ff16d921dd04bf728919ce7dab16cb426cf
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2023-08-23 23:07:18 +0530

    Add files via upload

---
+++ assignment 2/SizeOf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of long int: %lu bytes\n", sizeof(long int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+
+    return 0;
+}

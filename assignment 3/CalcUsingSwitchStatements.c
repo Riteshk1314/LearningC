a068f043c422f6e7a0d67489abb5737b3089394b
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2023-08-27 01:37:12 +0530

    Update CalcUsingSwitchStatements.c

--- assignment 3/CalcUsingSwitchStatements.c
+++ assignment 3/CalcUsingSwitchStatements.c
@@ -1,43 +1,4 @@
-#include <stdio.h>
 
-int main() {
-    int days, paise_total = 0, rupees_total = 0;
-
-    // Input: Taking days, paise, and rupees as input
-    scanf("%d %d %d", &days, &paise_total, &rupees_total);
-
-    if (days >= 1 && days <= 5) {
-        // Calculate paise for the given range directly using a formula
-        paise_total += days * 50;
-    } else if (days >= 6 && days <= 10) {
-        // Calculate paise and rupees separately for the given range
-        paise_total += 5 * 50 + (days - 5);
-        rupees_total += days - 5;
-    } else {
-        // Calculate paise and rupees separately for the last range
-        paise_total += 5 * 50 + 5 + (days - 10) * 10;
-        rupees_total += 5 + (days - 10);
-    }
-
-    // Output: Print the total amount in rupees and paise
-    printf("Amount to be paid is %d rupees and %d paise", rupees_total, paise_total);
-
-    return 0;
-}
-
-
-
-
-
-
-
-//CODE USING CONCEPT OF LOOP
-        ||
-        ||
-        ||
-        ||
-       \\//  
-        \/ 
             
 #include <stdio.h>
 

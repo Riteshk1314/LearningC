e392b69f7807c5178c9e631eb7491d6663d85edf
KrishnaSharma10 <154511673+KrishnaSharma10@users.noreply.github.com>
2024-02-17 16:42:03 +0530

    Update Pattern Question 1.c

--- Sessional Solutions/Pattern Question 1.c
+++ Sessional Solutions/Pattern Question 1.c
@@ -5,24 +5,26 @@
 // 4567654
 //567898765
 
-#include<stdio.h>
-int main()
-{
-    int i,j,k,l;
-    for(i=1;i<=5;i++)
-    {
-        for(j=1;j<=(5-i);j++)
-        {
+#include <stdio.h>
+
+int main(){
+    int i,j,k,row;
+    printf("Enter the no. of rows : ");
+    scanf("%d",&row);
+    for(i=1;i<=row;i++){
+        for(j=1;j<=row-i;j++){
             printf(" ");
         }
-        for(k=i;k<=(2*i-1);k++)
-        {
-            printf("%d",k);
-        }
-        for(l=(2*i-1)-1;l>=i;l--)
-        {
-            printf("%d",l);
+        for(j=1;j<=2*(i-1)+1;j++){
+            if(j<=i) printf("%d",i+j-1);
+            if(j>i){
+                for(k=2*(i-1);k>=i;k--){
+                    printf("%d",k);
+                }
+                break;
+            }
         }
         printf("\n");
     }
+    return 0;
 }
